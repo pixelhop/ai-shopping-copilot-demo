a25a8e5ad4a848ed3074ff587dcff50e4aff01c4
Jozef Maxted <hello@jozefmaxted.co.uk>
2024-07-04 22:19:05 +0100

    Implement checkout better

--- src/pages/PageCheckout.vue
+++ src/pages/PageCheckout.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { RadioGroup, RadioGroupOption } from '@headlessui/vue'
-import { CheckCircleIcon, TrashIcon } from '@heroicons/vue/20/solid'
+import { RadioGroup, RadioGroupOption } from '@headlessui/vue';
+import { CheckCircleIcon, TrashIcon } from '@heroicons/vue/20/solid';
+import { cart, totalPrice } from '../cart';
 
 const products = [
   {
@@ -202,33 +203,32 @@ const selectedDeliveryMethod = ref(deliveryMethods[0])
           <div class="mt-4 rounded-lg border border-gray-200 bg-white shadow-sm">
             <h3 class="sr-only">Items in your cart</h3>
             <ul role="list" class="divide-y divide-gray-200">
-              <li v-for="product in products" :key="product.id" class="flex px-4 py-6 sm:px-6">
+              <li v-for="product in cart" :key="product.id" class="flex px-4 py-6 sm:px-6">
                 <div class="flex-shrink-0">
-                  <img :src="product.imageSrc" :alt="product.imageAlt" class="w-20 rounded-md" />
+                  <img :src="product.image" :alt="product.name" class="w-20 rounded-md" />
                 </div>
 
                 <div class="ml-6 flex flex-1 flex-col">
                   <div class="flex">
                     <div class="min-w-0 flex-1">
                       <h4 class="text-sm">
-                        <a :href="product.href" class="font-medium text-gray-700 hover:text-gray-800">{{ product.title }}</a>
+                        <RouterLink :to="`/products/${id}`" class="font-medium text-gray-700 hover:text-gray-800">{{ product.name }}</RouterLink>
                       </h4>
-                      <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
-                      <p class="mt-1 text-sm text-gray-500">{{ product.size }}</p>
+                      <p class="mt-1 text-sm text-gray-500">x{{ product.qty }}</p>
                     </div>
 
-                    <div class="ml-4 flow-root flex-shrink-0">
+                    <!-- <div class="ml-4 flow-root flex-shrink-0">
                       <button type="button" class="-m-2.5 flex items-center justify-center bg-white p-2.5 text-gray-400 hover:text-gray-500">
                         <span class="sr-only">Remove</span>
                         <TrashIcon class="h-5 w-5" aria-hidden="true" />
                       </button>
-                    </div>
+                    </div> -->
                   </div>
 
                   <div class="flex flex-1 items-end justify-between pt-2">
-                    <p class="mt-1 text-sm font-medium text-gray-900">{{ product.price }}</p>
+                    <p class="mt-1 text-sm font-medium text-gray-900">&pound;{{ product.price }}</p>
 
-                    <div class="ml-4">
+                    <!-- <div class="ml-4">
                       <label for="quantity" class="sr-only">Quantity</label>
                       <select id="quantity" name="quantity" class="rounded-md border border-gray-300 text-left text-base font-medium text-gray-700 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
                         <option value="1">1</option>
@@ -240,13 +240,13 @@ const selectedDeliveryMethod = ref(deliveryMethods[0])
                         <option value="7">7</option>
                         <option value="8">8</option>
                       </select>
-                    </div>
+                    </div> -->
                   </div>
                 </div>
               </li>
             </ul>
             <dl class="space-y-6 border-t border-gray-200 px-4 py-6 sm:px-6">
-              <div class="flex items-center justify-between">
+              <!-- <div class="flex items-center justify-between">
                 <dt class="text-sm">Subtotal</dt>
                 <dd class="text-sm font-medium text-gray-900">$64.00</dd>
               </div>
@@ -257,10 +257,10 @@ const selectedDeliveryMethod = ref(deliveryMethods[0])
               <div class="flex items-center justify-between">
                 <dt class="text-sm">Taxes</dt>
                 <dd class="text-sm font-medium text-gray-900">$5.52</dd>
-              </div>
-              <div class="flex items-center justify-between border-t border-gray-200 pt-6">
+              </div> -->
+              <div class="flex items-center justify-between pt-6">
                 <dt class="text-base font-medium">Total</dt>
-                <dd class="text-base font-medium text-gray-900">$75.52</dd>
+                <dd class="text-base font-medium text-gray-900">&pound;{{ totalPrice }}</dd>
               </div>
             </dl>
 
